1a5eb30f285a2b536e8505faffa2f94a8bfa3aab
juanpantoja12345 <49863408+juanpantoja12345@users.noreply.github.com>
2021-10-04 11:34:22 +0200

    Add files via upload

---
+++ PracticaGit.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class PracticaGit {
+
+	public static Scanner teclado = new Scanner(System.in);
+	public static void main(String[] args) {
+	
+		System.out.println("Hola mundo");
+	}
+
+}
